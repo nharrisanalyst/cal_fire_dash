313dd161cac0ab72cac9e0cbcabdbfb6d24f7f03
Nathan Harris <nharrisanalyst@gmail.com>
2026-04-23 11:02:52 -0700

    mapAlertsToZipCode to filter alerts based on zipcode

--- cal_fire_dashboard/src/Alerts/hooks/utils/mapAlertsToZipcode.ts
+++ cal_fire_dashboard/src/Alerts/hooks/utils/mapAlertsToZipcode.ts
@@ -1,25 +1,35 @@
 import { type FireAlert } from '../useGetAlerts'
+import { loadZipcodes } from './loadZipcodes/loadZipcodes';
+
+export const filterAlertsByCname =(zipCnames:string[], alertCnames:string[]):boolean=>{
+  // const intersect = set_1.intersection(set_2);   2027 emcascript feature
+  // we are taking two sets and seeing if there is any instersection between the two
+  // this is a 2027 ecma feature todo in the future 
+    const set_1 = new Set(zipCnames);
+    const set_2 = new Set(alertCnames);
+    const instersection = [...set_1].filter(it=>set_2.has(it));
+
+    return instersection.length ===0?false:true;
+             
+}
+
+export const trimSAME =(SAME:string[]):string[]=>(
+  SAME.map(str=>str.slice(1))
+);
+
+
 
 export const mapAlertsToZipcode = async (zipcode:string, Alerts:FireAlert[]):Promise<FireAlert[]> =>{
-  const zipcodesResp =  await fetch('/json/zip_cname.json');
-  const zipcodes = await zipcodesResp.json();
+  const zipcodes =  await loadZipcodes();
   if(zipcodes[zipcode]===undefined) {
     throw new Error('zipcode supplied not found in zipcode json')
   }
   const zipCodeCnames = zipcodes[zipcode].cnames;
 
-  const zipFilteredAlerts = Alerts.filter(
-    a=>{
-      const cnames = a.geocode.SAME
-      const cnamesStripped = cnames.map(c=>c.slice(1));
-      for(let i=0; i<cnames.length; i++){
-        const cname = cnamesStripped[i];
-         if(zipCodeCnames.includes(cname)){
-          return true;
-         }
-      }
-      return false;
-    }
-  )
+  const zipFilteredAlerts = Alerts.filter(a=>{
+    const SAME = a.geocode.SAME; //SAME is CNAME data
+    const trimmedSame = trimSAME(SAME); // Trim becuase api send a leading indactor for the alert 
+    return filterAlertsByCname(zipCodeCnames, trimmedSame);
+  })
    return zipFilteredAlerts;
 }
