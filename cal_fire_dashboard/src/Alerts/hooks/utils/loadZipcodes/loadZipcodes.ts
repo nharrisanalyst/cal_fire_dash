f314adaddd628d36ed2cfc1748dd4f7f6ab04796
Nathan Harris <nharrisanalyst@gmail.com>
2026-04-23 23:07:42 -0700

    added a cache to for localfiles and added a cache to loading the localzipcode to CNAME data

--- cal_fire_dashboard/src/Alerts/hooks/utils/loadZipcodes/loadZipcodes.ts
+++ cal_fire_dashboard/src/Alerts/hooks/utils/loadZipcodes/loadZipcodes.ts
@@ -1,5 +1,15 @@
+import {localFilesCache} from '../../../../chache/LocalFilesCache'
+
+const ZPICODES_CACHE_KEY = 'ZIPCODES_ALREADY_LOADED';
+
 export const loadZipcodes= async ()=>{
+  //check cache if data is already there
+  if(localFilesCache.hasCache({key:ZPICODES_CACHE_KEY})) return localFilesCache.getCache({key:ZPICODES_CACHE_KEY});
+  //fetch data since it is not in the cache
   const zipResp = await fetch('/json/zip_cname.json');
-  return zipResp.json();
+  const zipcodeJSON = zipResp.json();
+  //cache data and return the data form the cache
+  localFilesCache.setCache({key:ZPICODES_CACHE_KEY, data:zipcodeJSON});
+  return localFilesCache.getCache({key:ZPICODES_CACHE_KEY});
 
 }
